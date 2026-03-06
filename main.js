963196a49787101d31f61cb698c1cea06d91c732
marjonamurodova-dev <marjonamurodova468@gmail.com>
2026-03-06 12:19:40 +0500

    final(06.03.2026/12:20)

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ let mainBurger = document.querySelector(".header__mb-buttons");
 let mainDiv = document.querySelector(".header__mb-div");
 let navButton = document.querySelector(".header__mb-button");
 let links = document.querySelectorAll(".header__mb-links-texts");
+let accordionBoxes = document.querySelectorAll(".faq__accardion-boxes");
+let accardionTexts = document.querySelectorAll(".faq__accardion-text");
+let accardionIcons = document.querySelectorAll(".faq__accardion-top-box-icon");
 function closeMenu() {
   mainDiv.classList.remove("active");
   openBurger.style.display = "block";
@@ -21,4 +24,12 @@ mainBurger.addEventListener("click", () => {
 navButton.addEventListener("click", closeMenu);
 links.forEach((link) => {
   link.addEventListener("click", closeMenu);
+});
+accordionBoxes.forEach((item, index) => {
+  let accardionText = accardionTexts[index];
+  let accardionIcon = accardionIcons[index];
+  item.addEventListener("click", () => {
+    accardionText.classList.toggle("active");
+    accardionIcon.classList.toggle("rotate");
+  });
 });
